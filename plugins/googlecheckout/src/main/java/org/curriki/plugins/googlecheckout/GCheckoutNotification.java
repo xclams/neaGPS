6f70be7375fa8d77c305cac7081f3692065332d3
Paul Libbrecht <paul@hoplahup.net>
2013-05-07 21:15:23 +0200

    Getting rid of probably unused things. paul

--- plugins/googlecheckout/src/main/java/org/curriki/plugins/googlecheckout/GCheckoutNotification.java
+++
@@ -1,7 +0,0 @@
-package org.curriki.plugins.googlecheckout;
-
-/** Data object representing the notification of the state of an order as received by the
- * Google Checkout web service calls (version 2.0).
- */
-public class GCheckoutNotification {
-}
